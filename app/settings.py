efbbde9160e243349ad323f3cb32d1a31af4017f
Maximos <capstone@anms.app>
2026-04-16 16:20:05 +0300

    Add django-cors-headers to fix CORS block

--- app/settings.py
+++ app/settings.py
@@ -54,12 +54,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework.authtoken',
     'rest_framework',
+    'corsheaders',
     'core',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
